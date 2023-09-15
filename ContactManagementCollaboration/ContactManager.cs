1154460ab1791a395083570f178941f996e60aed
rizaleff <rizaleffendi92@gmail.com>
2023-09-15 13:35:54 +0700

    Create Contact Manager Class

--- ContactManagementCollaboration/ContactManager.cs
+++ ContactManagementCollaboration/ContactManager.cs
@@ -8,5 +8,35 @@ namespace ContactManagementCollaboration
 {
     internal class ContactManager
     {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void CreateContact(string name, string phoneNumber, string emailAddress)
+        {
+            if (IsValidPhoneNumber(phoneNumber))
+            {
+                if (IsValidEmailAddress(emailAddress))
+                {
+                    Contact contact = new Contact(name, phoneNumber, emailAddress); 
+                }
+                else
+                {
+                    Console.WriteLine("Email Address Tidak Valid!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Phone Number Tidak Valid!");
+            }
+        }
+
+        private bool IsValidEmailAddress(string email)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+        }
+
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(phoneNumber, @"^\d{10}$");
+        }
     }
 }
